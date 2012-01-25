1bf1dd008c0101ad045dd561a6eef3089dbea1af
Jake Moffatt <jakeonrails@gmail.com>
2012-01-25 01:31:49 -0800

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foosball::Application.config.secret_token = '87478cb8d56d3e1f9a877ff63e23b7528d69b4cfd5cea2c3af13197199c118b59cddc96f210aaab2781afdf861b1c8dadb7c027d9ba1b91181d73b7dd6af0da8'
